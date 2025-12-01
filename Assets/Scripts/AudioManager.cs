d59efbc6741a8a83bd87194b0ff4387c0f57f5a5
Ellis <563319@escg.ac.uk>
2025-12-01 15:31:17 +0000

    UPDATE12

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -7,14 +7,15 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
 
-    public AudioClip[] clips;
-    AudioSource audioSource; //reference to the audio source component on the game object
+    //public AudioClip[] clips;
+    //AudioSource audioSource; //reference to the audio source component on the game object
 
 
 
     public Sound[] sounds;
     void Awake()
     {
+        //DontDestroyOnLoad(gameObject);
         // if instance is null, store a reference to this instance
         if (instance == null)
         {
@@ -27,22 +28,28 @@ public class AudioManager : MonoBehaviour
             // Another instance of this gameobject has been made so destroy it
             // as we already have one
             Destroy(gameObject);
+            return;
         }
-
-
 
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
-
             s.source.pitch = s.pitch;
-
+            s.source.loop = s.loop;
         }
 
 
-    }
 
+
+
+
+    }
+    private void Start()
+    {
+        
+    }
+    /*
     public void PlayClip(int clipNumber)
     {
         audioSource = GetComponent<AudioSource>();
@@ -54,9 +61,16 @@ public class AudioManager : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         audioSource.Stop(); //stop currently playing clip
     }
+    */
+
     public void Play (string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+        if ( s == null)
+        {
+            Debug.LogWarning("sound: " + name + " not found!");
+            return;
+        }
         s.source.Play();
     }
 
